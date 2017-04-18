799880ab4a3d1af2b40d46c5f7342145f7e90e90
Dmitry <mich.dima@gmail.com>
2017-04-19 00:36:06 +0300

    Fix collaboration work; add dynamic ami

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -13,26 +13,15 @@ resource "aws_instance" "db" {
     Name  = "${var.env}_${format("${var.name}%02d", count.index+1)}"
     Group = "${var.env}_${var.name}"
   }
-
-  provisioner "remote-exec" {
-    inline = "#Connected!"
-
-    connection {
-      agent       = false
-      type        = "ssh"
-      user        = "${var.ssh_user}"
-      private_key = "${file(var.private_key_path)}"
-    }
-  }
 }
 
 resource "aws_security_group" "db" {
-  name        = "example_db_sg"
-  description = "Allow HTTP access"
+  name        = "${var.env}_example_db_sg"
+  description = "Allow DB access"
 
   ingress {
-    from_port   = 0
-    to_port     = 80
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
