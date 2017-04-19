0daf423da3c9aeca87d58260a1297e209efcce6c
Artem Starostenko <artemstarostenko65@gmail.com>
2017-04-19 09:46:45 +0300

    add provision dependencies

--- terraform/modules/web/main.tf
+++ terraform/modules/web/main.tf
@@ -13,14 +13,25 @@ resource "aws_instance" "web" {
     Name  = "${var.env}_${format("${var.name}%02d", count.index+1)}"
     Group = "${var.env}_${var.name}"
   }
+
+  provisioner "remote-exec" {
+    inline = "#Connected!"
+
+    connection {
+      agent       = false
+      type        = "ssh"
+      user        = "${var.ssh_user}"
+      private_key = "${file(var.private_key_path)}"
+    }
+  }
 }
 
 resource "aws_security_group" "web" {
-  name        = "example_web_sg"
-  description = "Allow Mysql access"
+  name        = "${var.env}_example_web_sg"
+  description = "Allow HTTP access"
 
   ingress {
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
